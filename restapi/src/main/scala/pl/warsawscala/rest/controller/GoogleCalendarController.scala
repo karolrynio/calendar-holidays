8d200600d0d89f141c76680d0089a182e0843d79
andrzej.jozwik@gmail.com <andrzej.jozwik@gmail.com>
2016-06-01 20:50:08 +0200

    simplify api

--- restapi/src/main/scala/pl/warsawscala/rest/controller/GoogleCalendarController.scala
+++ restapi/src/main/scala/pl/warsawscala/rest/controller/GoogleCalendarController.scala
@@ -1,5 +1,6 @@
 package pl.warsawscala.rest.controller
 
+import java.util.concurrent.atomic.AtomicInteger
 import javax.inject.Singleton
 
 import com.typesafe.scalalogging.StrictLogging
@@ -7,15 +8,33 @@ import org.joda.time.DateTime
 import play.api.mvc.{Action, Controller, EssentialAction}
 
 @Singleton
-class GoogleCalendarController extends Controller with StrictLogging{
+class GoogleCalendarController extends Controller with StrictLogging {
 
-  var map = Map[String, (DateTime,DateTime)]()
+  val counter = new AtomicInteger()
 
+  var map = Map[Int, (DateTime, DateTime)]()
 
-  def holidays(startTime: DateTime, endTime: DateTime): EssentialAction = Action {
+  val START = "start"
+  val END = "end"
+  val CODE = "code"
+  val STATE = "state"
+
+  def holidays(): EssentialAction = Action {
     r =>
+      val startTime = r.queryString.get(START).map(str => DateTime.parse(str.head)).getOrElse(DateTime.now().withMonthOfYear(1).withTimeAtStartOfDay().withDayOfMonth(1))
+      val endTime = r.queryString.get(END).map(str => DateTime.parse(str.head)).getOrElse(DateTime.now().withMonthOfYear(12).withTimeAtStartOfDay().withDayOfMonth(31))
       logger.debug(s"$startTime $endTime")
-      Redirect(s"http://www.google.pl/$startTime/$endTime")
+      r.queryString.get(STATE) match {
+        case Some(state) =>
+          val id = state.head.toInt
+          NotFound(s"$id")
+        case None =>
+          map = map + (counter.incrementAndGet() -> (startTime,endTime))
+          Ok(s"$startTime $endTime")
+          //Redirect(s"http://local/$startTime/$endTime")
+      }
+
+
   }
 
 }
