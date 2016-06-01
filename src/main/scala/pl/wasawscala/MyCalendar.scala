39491a86f4286c46d186a36684c18c8b4f9c00c9
Jacob Martin <jakub.wit.martin@gmail.com>
2016-06-01 20:27:10 +0200

    Mock

--- src/main/scala/pl/wasawscala/MyCalendar.scala
+++ src/main/scala/pl/wasawscala/MyCalendar.scala
@@ -2,6 +2,8 @@ package pl.warsawscala
 
 import java.time.LocalDate
 
+import scala.concurrent.Future
+
 trait MyCalendar {
   def getEventsFor(from: LocalDate, to: LocalDate): Future[Seq[PlannedEvent]] // ???
 }
@@ -10,21 +12,21 @@ object MyCalendar {
   //def apply(Code: String): MyCalendar = MyCalendarImp(Code)
   def apply(Code: String): MyCalendar = MyCalendarStub()
 }
-
-case class MyCalendarImp(Code: String) {
-  def getEventsFor(from: LocalDate, to: LocalDate): Future[Seq[PlannedEvent]] {
-
+/*
+case class MyCalendarImp(Code: String) extends MyCalendar {
+  def getEventsFor(from: LocalDate, to: LocalDate): Future[Seq[PlannedEvent]] = {
   }
 }
+*/
 
-case class MyCalendarStub() {
-  def getEventsFor(from: LocalDate, to: LocalDate): Future[Seq[PlannedEvent]] {
+case class MyCalendarStub() extends MyCalendar {
+  def getEventsFor(from: LocalDate, to: LocalDate): Future[Seq[PlannedEvent]] = {
     Future {
-    val myPlannedEvent: PlannedEvent = PlannedEvent (new LocalDate (2016, 3, 1), new LocalDate (2017, 3, 10), List ("holiday", "icecreamdays") )
-    val myPlannedEvent2: PlannedEvent = PlannedEvent (new LocalDate (2016, 5, 5), new LocalDate (2016, 5, 10), List ("grilldays") )
-    val myPlannedEvent3: PlannedEvent = PlannedEvent (new LocalDate (2016, 7, 1), new LocalDate (2016, 7, 5), List ("holiday") )
-    val myFuture: Future[Seq[PlannedEvent]] = Future (List (myPlannedEvent, myPlannedEvent2, myPlannedEvent3) )
-  }
+      val myPlannedEvent: PlannedEvent = PlannedEvent(new LocalDate(2016, 3, 1), new LocalDate(2017, 3, 10), List("holiday", "icecreamdays"))
+      val myPlannedEvent2: PlannedEvent = PlannedEvent(new LocalDate(2016, 5, 5), new LocalDate(2016, 5, 10), List("grilldays"))
+      val myPlannedEvent3: PlannedEvent = PlannedEvent(new LocalDate(2016, 7, 1), new LocalDate(2016, 7, 5), List("holiday"))
+      List(myPlannedEvent, myPlannedEvent2, myPlannedEvent3)
+    }
   }
 }
 
